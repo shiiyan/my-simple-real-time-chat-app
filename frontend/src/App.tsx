649bbae1c29e0fc4d8331db5ffee11815ff45709
shiiyan <36617009+shiiyan@users.noreply.github.com>
2023-12-03 22:41:26 +0900

    feat: add router for chat and login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,48 @@
 import { Flex, Heading } from "@chakra-ui/react";
 import "./App.css";
 import Chat from "./Chat";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import { ReactElement } from "react";
+
+function isAuthenticated() {
+  return false;
+}
+
+const ProtectedRoute: React.FC<{ children: ReactElement }> = ({ children }) => {
+  return isAuthenticated() ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Flex direction="column" height="100vh" bg="gray.100" p={4}>
         <Heading as="h1" textAlign="center" mb={4}>
           Simple Chat App
         </Heading>
-        <Chat />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/chat"
+            element={
+              <ProtectedRoute>
+                <Chat />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              isAuthenticated() ? (
+                <Navigate to="/chat" />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+        </Routes>
       </Flex>
-    </div>
+    </BrowserRouter>
   );
 }
 
