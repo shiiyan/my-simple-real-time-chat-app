8be9616d7cd60bf7fcef58431f08615b95fa9edd
shiiyan <36617009+shiiyan@users.noreply.github.com>
2023-12-04 09:31:41 +0900

    feat: set error message when login failed

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -5,15 +5,19 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Text,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const currentClientId = "3e00193e-c52d-4340-b040-7b1be9a1ef77";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
 
   const handleLogin = async () => {
     try {
@@ -29,15 +33,27 @@ function Login() {
           },
         }
       );
-      localStorage.setItem("token", response.data.token);
-    } catch {
-      console.error("Login failed");
+
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.token);
+        navigate("/chat");
+      } else {
+        setError("Login failed. Please try again.");
+      }
+    } catch (error) {
+      setError("Login failed. Please try again.");
+      console.error("Login failed", error);
     }
   };
 
   return (
     <Flex height="100%" alignItems="center" justifyContent="center">
       <Box bg="white" p={6} rounded="md" w="100%" maxW={400}>
+        {error && (
+          <Text color="red.500" mb={4}>
+            {error}
+          </Text>
+        )}
         <FormControl id="username" isRequired>
           <FormLabel>Username</FormLabel>
           <Input
